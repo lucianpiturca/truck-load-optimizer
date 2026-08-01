342045c7a0b701a5cda3db0db96a53797fed919a
lucianpiturca <lucian.piturca@gmail.com>
2026-08-01 14:47:29 +0300

    Delete constants.py

--- constants.py
+++
@@ -1,6 +0,0 @@
-PALLET_TYPES = {
-    "Euro": {"length": 1.20, "width": 0.80},
-    "Industrial": {"length": 1.20, "width": 1.00},
-    "Half": {"length": 0.80, "width": 0.60},
-    "Custom": None,
-}
